0d468b77dc746412f10e18fcc59f9a7ec0d7081d
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-07-20 03:07:20 +0530

    Added a reset button which will reset the score of both the team

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -14,24 +14,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int a=0;
+    int a = 0;
 
 
     /***************************************************
      * method for A
      ****************************************************/
-    public void addThreeA(View view){
-        a +=3;
+    public void addThreeA(View view) {
+        a += 3;
         ThreeA(a);
     }
 
-    public void addTwoA(View view){
-        a +=2;
+    public void addTwoA(View view) {
+        a += 2;
         ThreeA(a);
     }
 
-    public void addFreeA(View view){
-        a +=1;
+    public void addFreeA(View view) {
+        a += 1;
         ThreeA(a);
     }
 
@@ -42,30 +42,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /**
+    /*********************************************
      * method for B
-     */
+     *********************************************/
 
-    int b=0;
+    int b = 0;
 
-    public void addThreeB(View view){
-        b +=3;
+    public void addThreeB(View view) {
+        b += 3;
         ThreeB(b);
     }
 
-    public void addTwoB(View view){
-        b +=2;
+    public void addTwoB(View view) {
+        b += 2;
         ThreeB(b);
     }
 
-    public void addFreeB(View view){
-        b +=1;
+    public void addFreeB(View view) {
+        b += 1;
         ThreeB(b);
     }
 
     private void ThreeB(int i) {
         TextView t = (TextView) findViewById(R.id.B);
         t.setText("" + i);
+    }
+
+
+    /***********************************************
+     * Method for reset
+     ***********************************************/
+
+    public void ResetAB(View view) {
+        a = 0;
+        b = 0;
+        clearA(a);
+        clearB(b);
+
+    }
+
+    private void clearA(int i) {
+        TextView t = (TextView) findViewById(R.id.A);
+        t.setText("" + i);
+
+    }
+
+    private void clearB(int i) {
+        TextView t = (TextView) findViewById(R.id.B);
+        t.setText("" + i);
+
     }
 
 
